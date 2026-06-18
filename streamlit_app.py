374243d289c198dbeadb507c3d7fe37f933497c1
Michelle Lauren <micca9304@gmail.com>
2026-06-19 00:03:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,18 @@ import streamlit as st
 import joblib
 import traceback
 
-st.title("Pickle Debug")
+st.title("Loading Both Models")
 
 try:
-    obj = joblib.load("autoimmune_system_set2_final.pkl")
-    st.success("✅ Dataset 2 loaded successfully!")
-    st.write(type(obj))
-    st.write(obj.keys())
+    set1 = joblib.load("autoimmune_system_set1_final.pkl")
+    st.success("✅ Dataset 1 loaded")
+except Exception:
+    st.error("❌ Dataset 1 failed")
+    st.code(traceback.format_exc())
 
-except Exception as e:
-    st.error("❌ Failed to load Dataset 2")
+try:
+    set2 = joblib.load("autoimmune_system_set2_final.pkl")
+    st.success("✅ Dataset 2 loaded")
+except Exception:
+    st.error("❌ Dataset 2 failed")
     st.code(traceback.format_exc())
